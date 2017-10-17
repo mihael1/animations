02eddda5e8d1e3f88f0c3c870ef0eee37ce9d987
Mihael Šandro <Mihael@mihaels-mbp.home>
2017-10-17 21:14:52 +0200

    First scene_just CSS

---
+++ java.js
@@ -0,0 +1,8 @@
+/*window.addEventListener("load", pageReady);
+function pageReady(){
+    console.log("all loaded");
+}
+
+let startAnim = document.querySelector(".start");
+startAnim.addEventListener("click", startAnimations);
+function startAnimations*/
