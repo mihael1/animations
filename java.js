d38b076d4fdee28e8da6f5605663a2e1b09a8c79
Mihael Šandro <Mihael@mihaels-mbp.home>
2017-10-18 21:12:35 +0200

    audio added

--- java.js
+++ java.js
@@ -1,10 +1,21 @@
 let back = document.querySelector("#newback");
 let startAnim = document.querySelector(".start");
+let hero = document.querySelector(".man")
+let aud = document.querySelector("#morningSound");
+aud.play();
+
+let audwater = document.querySelector("#waterSound")
+audwater.play();
+audwater.loop = true;
 startAnim.addEventListener("click", startAnimations);
 
 function startAnimations() {
     console.log("next scene please");
-    back.classList.add("#newbackground");
+    aud.addEventListener('ended', soundEnded);
+}
+
+function soundEnded() {
+    console.log("Rooster sound ended..")
 }
 
 function backgroundNew() {
@@ -12,6 +23,24 @@ function backgroundNew() {
     if (x.style.display === "none") {
 
     } else {
-       x.style.display = "block";
+        x.style.display = "block";
     }
 }
+
+
+
+/* let horse = document.querySelector("#horse");
+  let horseSound = document.querySelector("#horseSound");
+
+  horse.addEventListener("click", playSound);
+
+  function playSound(){
+    horseSound.volume = .5;
+    horseSound.play();
+    horseSound.addEventListener('ended', soundEnded);
+  }
+
+  function soundEnded(){
+    console.log("The sound ended :-(")
+  }
+  */
